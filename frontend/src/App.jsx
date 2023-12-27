13c7175dfc1a111ca383ba3d44e97431692e6e8a
Lorenz Gil Suico <19101360@usc.edu.ph>
2023-12-27 16:52:30 +0800

    Testing Pushing Command

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { Component, useState } from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+
+  componentDidMount(){
+
+  }
+  componentWillUnmount(){
+    
+  }
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App
