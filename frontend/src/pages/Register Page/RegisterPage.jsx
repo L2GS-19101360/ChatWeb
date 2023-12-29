8d183e00ad1134a1414572214317be698b7b0588
Lorenz Gil Suico <19101360@usc.edu.ph>
2023-12-29 17:10:53 +0800

    Need to Link Register Page Backend

--- frontend/src/pages/Register Page/RegisterPage.jsx
+++ frontend/src/pages/Register Page/RegisterPage.jsx
@@ -10,7 +10,15 @@ class RegisterPage extends Component {
     constructor() {
         super();
         this.state = {
-            showPassword: false
+            showPassword: false,
+
+            newFirstName: "",
+            newLastName: "",
+            newContactNumber: "",
+            newUsername: "",
+            newEmail: "",
+            newPassword: "",
+            tempImage: "#%&{}>"
         }
         this.toApp = this.toApp.bind(this);
     }
@@ -27,8 +35,21 @@ class RegisterPage extends Component {
             showPassword: !prevState.showPassword
         }));
     }
-    toApp(){
+    toApp() {
         this.props.history.push('/')
+    }
+
+    handleRegisterAccount = (event) => {
+        event.preventDefault()
+
+        console.log(this.state.newFirstName);
+        console.log(this.state.newLastName);
+        console.log(this.state.newUsername);
+        console.log(this.state.newContactNumber);
+        console.log(this.state.newEmail);
+        console.log(this.state.newPassword);
+        console.log(this.state.tempImage);
+
     }
 
     render() {
@@ -42,21 +63,33 @@ class RegisterPage extends Component {
                         <img src={webname} alt="" id='webnameLogin' onClick={this.toApp} style={{ cursor: 'pointer' }} />
                     </div>
                     <div className='rightRegister'>
-                        <h1 style={{marginBottom: '20px'}}>Register Page</h1>
-                        <div className='infoName' style={{marginBottom: '20px'}}>
-                            <input className="form-control" type="text" placeholder="Enter First Name" aria-label="default input example" style={{ marginRight: '5px' }} />
-                            <input className="form-control" type="text" placeholder="Enter Last Name" aria-label="default input example" style={{ marginLeft: '5px' }} />
-                        </div>
-                        <input className="form-control" type="tel" placeholder="Enter Contact Number" aria-label="default input example" style={{marginBottom: '20px'}} />
-                        <input className="form-control" type="text" placeholder="Enter Username" aria-label="default input example" style={{marginBottom: '20px'}} />
-                        <input className="form-control" type="email" placeholder="Enter Email" aria-label="default input example" style={{marginBottom: '20px'}} />
-                        <div className='input-group'>
-                            <input className='form-control' type={inputType} placeholder='Enter Password' aria-label='Password' style={{ marginBottom: '25px' }} />
-                            <span className='input-group-text' style={{ marginBottom: '25px' }}>
-                                <i className={eyeIcon} onClick={this.togglePassword}></i>
-                            </span>
-                        </div>
-                        <p>Already Have an Account? <Link to='/LoginPage'>Login Account</Link> </p>
+                        <h1 style={{ marginBottom: '10px' }}>Register Page</h1>
+
+                        <form action="" method="post">
+
+                            <div className='infoName' style={{ marginBottom: '10px' }}>
+                                <input className="form-control" type="text" placeholder="Enter First Name" aria-label="default input example" style={{ marginRight: '5px' }} value={this.state.newFirstName} onChange={(e) => this.setState({ newFirstName: e.target.value })} />
+
+                                <input className="form-control" type="text" placeholder="Enter Last Name" aria-label="default input example" style={{ marginLeft: '5px' }} value={this.state.newLastName} onChange={(e) => this.setState({newLastName: e.target.value})}/>
+                            </div>
+                            <input className="form-control" type="tel" placeholder="Enter Contact Number" aria-label="default input example" style={{ marginBottom: '10px' }} value={this.state.newContactNumber} onChange={(e) => this.setState({newContactNumber: e.target.value})}/>
+
+                            <input className="form-control" type="text" placeholder="Enter Username" aria-label="default input example" style={{ marginBottom: '10px' }} value={this.state.newUsername} onChange={(e) => this.setState({newUsername: e.target.value})}/>
+
+                            <input className="form-control" type="email" placeholder="Enter Email" aria-label="default input example" style={{ marginBottom: '10px' }} value={this.state.newEmail} onChange={(e) => this.setState({newEmail: e.target.value})}/>
+
+                            <div className='input-group'>
+                                <input className='form-control' type={inputType} placeholder='Enter Password' aria-label='Password' style={{ marginBottom: '20px' }} value={this.state.newPassword} onChange={(e) => this.setState({newPassword: e.target.value})}/>
+                                <span className='input-group-text' style={{ marginBottom: '20px' }}>
+                                    <i className={eyeIcon} onClick={this.togglePassword}></i>
+                                </span>
+                            </div>
+
+                            <button type="submit" className="btn btn-primary" onClick={this.handleRegisterAccount}>Register Account</button>
+
+                        </form>
+
+                        <p style={{marginTop: '15px'}}>Already Have an Account? <Link to='/LoginPage'>Login Account</Link> </p>
                     </div>
                 </div>
             </div>
