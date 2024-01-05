f4934bba9c189419c2f5ca15b8628ed0bcdf23c9
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-01-05 23:09:31 +0800

    Logout Account

--- frontend/src/pages/Home Page/HomePage.jsx
+++ frontend/src/pages/Home Page/HomePage.jsx
@@ -1,27 +1,27 @@
-import { Component, useState } from 'react'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import weblogo from '../../assets/weblogo.jpg'
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import weblogo from '../../assets/weblogo.jpg';
 import LetteredAvatar from '../../components/LetterAvatar';
 
 class HomePage extends Component {
-
     constructor() {
         super();
-
         const userDataJSON = sessionStorage.getItem('userData');
         const userData = userDataJSON ? JSON.parse(userDataJSON) : null;
-
         this.state = {
             userData: userData
-        }
+        };
     }
 
     componentDidMount() {
         console.log(this.state.userData);
     }
-    componentWillUnmount() {
 
+    componentWillUnmount() {
+        // Add any cleanup code if needed
+        sessionStorage.removeItem('userData');
+        window.location.href = "/";
     }
 
     render() {
@@ -35,13 +35,24 @@ class HomePage extends Component {
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <LetteredAvatar name={`${this.state.userData.firstname} ${this.state.userData.lastname}`} size={63}/>  
+                        <div data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                            <LetteredAvatar name={`${this.state.userData.firstname} ${this.state.userData.lastname}`} size={63} />
+                        </div>
                     </div>
                 </nav>
+
+                <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <button type="button" className="btn btn-danger" onClick={this.componentWillUnmount}>Logout Account</button>
+                    </div>
+                </div>
             </div>
         );
     }
-
 }
 
-export default HomePage
+export default HomePage;
